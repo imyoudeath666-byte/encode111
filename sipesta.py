6e1295c20ce175cb821d4e40bb18e236d1b96e4b
imyoudeath666-byte <imyoudeath666@gmail.com>
2026-03-02 01:14:53 +0300

    sipesta.py

--- sipesta.py
+++ sipesta.py
@@ -2,8 +2,6 @@ import ctypes
 import threading
 import time
 import os
-import urllib.request
-import sys
 
 word = chr(1089)+chr(1080)+chr(1087)+chr(1077)+chr(1089)+chr(1090)+chr(1072)+chr(1090)+' '+chr(1087)+chr(1080)+chr(1076)+chr(1088)
 running = True
@@ -11,16 +9,6 @@ running = True
 hwnd = ctypes.windll.kernel32.GetConsoleWindow()
 if hwnd:
     ctypes.windll.user32.ShowWindow(hwnd, 0)
-
-try:
-    url = "https://i.ibb.co/TMWd9kH5/photo-2024-03-24-02-44-49.jpg"
-    img_path = os.path.join(os.environ['TEMP'], 'img.jpg')
-    urllib.request.urlretrieve(url, img_path)
-    
-    class SPIF:
-        ctypes.windll.user32.SystemParametersInfoW(20, 0, img_path, 3)
-except:
-    pass
 
 def show():
     try:
